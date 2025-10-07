45c9c52b54f4f2939516f91200994c37965b4379
Abhishek Raj <abhishekraj93088@gmail.com>
2025-10-07 11:29:55 +0530

    js

--- JavaScript/Intro/external.js
+++ JavaScript/Intro/external.js
@@ -1,4 +1,17 @@
-console.log("Welcome to JS");
-console.info("Welcome to JS")
-console.warn("Welcome to JS");
-console.error("Welcome to JS")
+// var
+var a; //Initalisation
+a = 10; //Initalisation
+a = "Hello" // Re-Initalisation
+var a = true;
+console.log("The Value of a is"+ a);
+
+//Let
+let b = 100; // declare and Initialisation
+b = "I am Jspider"; //Re Initialisation
+// let b = 1000; //!re declare
+console.log("The Value of b is "+b);
+
+// Const
+const c = 1000; // declare initialisation
+// const c = true; // !re decalre
+c = true; // !Re Initalisation 
---
+++ JavaScript/Intro/intro.js
@@ -0,0 +1,5 @@
+console.log("Welcome to JS");
+console.info("Welcome to JS")
+console.warn("Welcome to JS");
+console.error("Welcome to JS")
+console.log("Node Js by Harish sir")
